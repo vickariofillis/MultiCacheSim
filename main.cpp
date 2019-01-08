c51a5470f89ede872ba5f5ba7c90331ef72f8536
Victor Kariofillis <vickariofillis@gmail.com>
2019-01-08 17:26:35 -0500

    minor fixes 1.4

--- main.cpp
+++ main.cpp
@@ -82,6 +82,7 @@ int main(int argc, char* argv[])
    uint64_t address;
    unsigned long long lines = 0;
 
+   zstr::ifstream infile("/aenao-99/karyofyl/results/pin/pinatrace/parsec/test/small/pkgs/apps/test/run/trace.out.gz");
    if (suite == "parsec") {
         std::string type;
         if (benchmark == "blackscholes" || benchmark == "bodytrack" || benchmark == "facesim" || benchmark == "ferret" || benchmark == "fluidanimate" || benchmark == "freqmine" ||
